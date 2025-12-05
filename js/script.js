c4b37120df4c38c5706a98ebf97c2c4fa55d0049
xyxandras-design <x.yxandras@gmail.com>
2025-12-05 09:23:04 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -51,9 +51,11 @@ function goBack(fallbackUrl = 'index.html') {
       const refOrigin = new URL(document.referrer).origin;
       if (refOrigin === location.origin) {
         window.history.back();
-      setTimeout(() => {
-      window.location.href = fallbackUrl;
-    }, 300);
+        setTimeout(() => {
+          if (location.href === location.href) {
+            window.location.href = fallbackUrl;
+          }
+        }, 300);
         return;
       }
     }
