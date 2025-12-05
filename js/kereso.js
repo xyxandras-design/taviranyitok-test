6706dc2cc343498ced57d99ec8a7669848f5c65b
xyxandras-design <x.yxandras@gmail.com>
2025-12-05 09:19:24 +0100

    Add files via upload

--- js/kereso.js
+++ js/kereso.js
@@ -127,7 +127,7 @@ function showSuggestions(list) {
     div.addEventListener("click", () => {
       const folderName = item.gyarto.toLowerCase() + "-taviranyitok";
       const url = `${folderName}/${item.html}`;
-      window.location.href = url;
+      window.open(url, "_blank");  // ← itt a változtatás
     });
     box.appendChild(div);
   });
